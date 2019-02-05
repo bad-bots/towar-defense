0b5fcf0bc989969fbf095f5d45d72266c83f8998
unknown <duncangichimu@gmail.com>
2019-02-05 10:43:26 -0800

    (feature) Add SVG materials

---
+++ TowAR Defense/Assets/Scripts/Pages/EndGamePageController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGamePageController : MonoBehaviour
+{
+    // public SceneField gameScene;
+
+    // void Start()
+    // {
+    //     NetworkManager.instance.DisplayEndGameEvent += OnGameEnd;
+    // }
+
+    // void OnGameEnd(string winner)
+    // {
+    //     SceneManager.sceneLoaded += OnSceneLoad;
+    //     UnityEngine.SceneManagement.SceneManager.LoadScene(gameScene);
+    //     GetComponentInChildren<Text>().text = winner + " Wins";
+    // }
+
+    // private void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    // {
+    //     SceneManager.sceneLoaded -= OnSceneLoad;
+    //     var gameCon = GameObject.FindGameObjectWithTag("GameController")
+    //           .GetComponent<GameController>();
+    // }
+}
