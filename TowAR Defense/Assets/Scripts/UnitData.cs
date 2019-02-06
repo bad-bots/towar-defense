ca189bf43ee2555d66270d8008e90d23e18a549c
Scott Techau <scotttechau@gmail.com>
2019-02-06 14:29:15 -0800

    fix(target): Fix death animation causing targeting to break.

--- TowAR Defense/Assets/Scripts/UnitData.cs
+++ TowAR Defense/Assets/Scripts/UnitData.cs
@@ -5,47 +5,52 @@ using UnityEngine;
 [RequireComponent(typeof(HealthBehaviour))]
 public class UnitData : MonoBehaviour
 {
-  public UnitType type;
-  public float currentHealth;
-  public int playerNo = 1;
-  public int unitId;
-  Animator m_Animator;
+    public UnitType type;
+    public float currentHealth;
+    public int playerNo = 1;
+    public int unitId;
+    Animator m_Animator;
 
-  private HealthBehaviour m_healthBehaviour;
-  private float previousHealth;
+    private HealthBehaviour m_healthBehaviour;
+    private float previousHealth;
 
-  public int enemyPlayerNo
-  {
-    get
+    public int enemyPlayerNo
     {
-      return 3 - playerNo;
+        get
+        {
+            return 3 - playerNo;
+        }
     }
-  }
 
-  void Start()
-  {
-    currentHealth = type.maxHealth;
-    m_Animator = gameObject.GetComponent<Animator>();
-    m_healthBehaviour = GetComponent<HealthBehaviour>();
-    m_healthBehaviour.maxHealth = type.maxHealth;
-    m_healthBehaviour.health = type.maxHealth;
-  }
-
-  void Update()
-  {
-    m_healthBehaviour.health = currentHealth;
-    if (currentHealth <= 0 && currentHealth != previousHealth)
+    void Start()
     {
-      if(!GameController.instance.unitSpawner.RemoveUnit(this))
-        Debug.LogWarning("unit not removed");
-      m_Animator.SetBool("Dead", true);
-      Invoke("DestroyUnit", 3);
+        currentHealth = type.maxHealth;
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_healthBehaviour = GetComponent<HealthBehaviour>();
+        m_healthBehaviour.maxHealth = type.maxHealth;
+        m_healthBehaviour.health = type.maxHealth;
     }
-    previousHealth = currentHealth;
-  }
 
-  void DestroyUnit()
-  {
-    Destroy(gameObject);
-  }
+    void Update()
+    {
+        m_healthBehaviour.health = currentHealth;
+        if (currentHealth <= 0 && currentHealth != previousHealth)
+        {
+            if (!GameController.instance.unitSpawner.RemoveUnit(this))
+                Debug.LogWarning("unit not removed");
+            m_Animator.SetBool("Dead", true);
+            Invoke("DestroyUnit", 3);
+        }
+        previousHealth = currentHealth;
+    }
+
+    void OnDestroy()
+    {
+        GameController.instance.unitSpawner.RemoveUnit(this);
+    }
+
+    void DestroyUnit()
+    {
+        Destroy(gameObject);
+    }
 }
