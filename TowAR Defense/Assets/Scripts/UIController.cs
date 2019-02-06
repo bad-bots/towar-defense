f16df35ad9ce847c5a7d1a23311723adc4b2656f
Preston Wallace <wallace.preston@gmail.com>
2019-02-05 16:47:10 -0800

    feature(button): Cooldownworking cooldown that disables button

--- TowAR Defense/Assets/Scripts/UIController.cs
+++ TowAR Defense/Assets/Scripts/UIController.cs
@@ -1,31 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class UIController : MonoBehaviour
 {
-    private bool cooldown = false;
 
+    public Button[] spawnUnitButtonsType;
+    public Button spawnUnitButton;
+    void Awake()
+    {
+        spawnUnitButtonsType = GetComponentsInChildren<Button>();
+    }
     public void SpawnKnight()
     {
         GameController.instance.RequestSpawnUnit("Knight");
     }
-
     public void SpawnUnit(string unitType)
     {
-        if ( cooldown == false ) {
-            //Do something
-            GameController.instance.RequestSpawnUnit(unitType);
-            Invoke("ResetCooldown",2.0f);
-            cooldown = true;
-     }
+        //Do something
+        GameController.instance.RequestSpawnUnit(unitType);
+        foreach(var button in spawnUnitButtonsType)
+        {
+            if(button.name.StartsWith("spawn" + unitType, true, System.Globalization.CultureInfo.CurrentCulture))
+            {
+                button.interactable = false;
+                StartCoroutine(ResetCooldown(2.0f, button));
+            };
+        }
+
     }
 
     public void DebugSpawnNight()
     {
         GameController.instance.RequestDebugSpawnUnit();
     }
-    void ResetCooldown(){
-     cooldown = false;
- }
+    IEnumerator ResetCooldown(float cooldown, Button button)
+    {
+        yield return new WaitForSeconds(cooldown);
+        button.interactable = true;
+    }
 }
