ca881b52e78770f211ce2e3b7a469e94008ba959
unknown <duncangichimu@gmail.com>
2019-02-05 18:29:26 -0800

    (chore) Reduce knight speed back to original speed

--- TowAR Defense/Assets/Scripts/Pages/GamePendingPageController.cs
+++ TowAR Defense/Assets/Scripts/Pages/GamePendingPageController.cs
@@ -4,22 +4,4 @@ using UnityEngine;
 
 public class GamePendingPageController : MonoBehaviour
 {
-    IEnumerator Load()
-    {
-        #if UNITY_IPHONE
-        Handheld.SetActivityIndicatorStyle(iOS.ActivityIndicatorStyle.Gray);
-        #elif UNITY_ANDROID
-        Handheld.SetActivityIndicatorStyle(AndroidActivityIndicatorStyle.Small);
-        #endif
-
-        Handheld.StartActivityIndicator();
-        yield return new WaitForSeconds(0);
-    }
-
-    void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 10, 200, 200), "Load!"))
-            StartCoroutine(Load());
-    }
-
 }
