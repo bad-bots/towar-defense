3dc46cc0959226ba7b3dcd20d8bc8376b917e618
Matt Clark <mattwc93@gmail.com>
2019-02-08 14:49:16 -0800

    unit balancing

--- server/app/memDb/units.js
+++ server/app/memDb/units.js
@@ -5,11 +5,11 @@ const Unit = db.addCollection("units");
 Unit.unitCost = function(type) {
   switch (type) {
     case "archer":
-      return 100;
-    case "knight":
       return 150;
+    case "knight":
+      return 100;
     case "phallanx":
-      return 200;
+      return 125;
     default:
       return 100;
   }
@@ -18,19 +18,33 @@ Unit.unitCost = function(type) {
 Unit.unitDamage = function(type) {
   switch (type) {
     case "archer":
-      return 100;
-    case "knight":
       return 150;
+    case "knight":
+      return 125;
     case "phallanx":
-      return 200;
+      return 100;
     default:
       return 100;
   }
 };
 
+getMaxHealth = function(type) {
+  switch (type) {
+    case "archer":
+      return 500;
+    case "knight":
+      return 750;
+    case "phallanx":
+      return 1050;
+    default:
+      return 500;
+  }
+};
+
 Unit.createUnit = function(playerNo, unitType, position, rotation) {
+  let health = getMaxHealth(unitType);
   return this.insert({
-    health: 200,
+    health,
     position,
     rotation,
     unitType,
